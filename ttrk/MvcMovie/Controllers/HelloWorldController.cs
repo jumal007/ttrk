42cc87eac7e8d1b0c12d4e5e367845a8c157e9e6
Ilja Freiberg <ilfrei@ttu.ee>
2019-04-29 13:22:16 +0300

    comicbookgallery start

--- ttrk/MvcMovie/Controllers/HelloWorldController.cs
+++ ttrk/MvcMovie/Controllers/HelloWorldController.cs
@@ -13,12 +13,11 @@ namespace MvcMovie.Controllers
             return "This is my default action...";
         }
 
-        // 
         // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome()
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
