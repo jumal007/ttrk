2cff50b43530fa243d7f7f6469a9d1ba32a20ea2
Ilja Freiberg <ilfrei@ttu.ee>
2019-05-09 15:34:22 +0300

    mvc movie stage 4 (data base)

---
+++ ttrk/ttrk/Data/book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ttrk.Data
+{
+    public class @book
+    {
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
