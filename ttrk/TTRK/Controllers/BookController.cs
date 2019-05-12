32a5ce4a9d8376102bfd4b6be7c4108e7709c64a
Ilja Freiberg <ilfrei@ttu.ee>
2019-05-12 14:39:02 +0300

    we start our project again

---
+++ ttrk/TTRK/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TTRK.Controllers
+{
+    public class BookController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return Content("Hello from lazzy world");
+        }
+    }
+}
